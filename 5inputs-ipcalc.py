bb45c1ed75d1ea561e3dbf3f18f2f39aed5ac1e1
S3thBr0wn <31544947+S3thBr0wn@users.noreply.github.com>
2018-03-13 10:13:35 -0700

    Update 5inputs-ipcalc.py

--- 5inputs-ipcalc.py
+++ 5inputs-ipcalc.py
@@ -1,75 +1,107 @@
 def getsubnet(cidr,nmask):
-	print('cidr',cidr)
-	c =16
-	if(cidr == 32):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 255;nmask[3] = 255;
-	if(cidr == 31):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 255;nmask[3] = 254;
-	if(cidr == 30):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 255;nmask[3] = 252;
-	if(cidr == 29):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 255;nmask[3] = 248;
-	if(cidr == 28):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 255;nmask[3] = 240;
-	if(cidr == 27):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 255;nmask[3] = 224;
-	if(cidr == 26):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 255;nmask[3] = 192;
-	if(cidr == 25):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 255;nmask[3] = 128;
-	if(cidr == 24):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 255;nmask[3] = 0;
-	if(cidr == 23):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 254;nmask[3] = 0;
-	if(cidr == 22):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 252;nmask[3] = 0;
-	if(cidr == 21):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 248;nmask[3] = 0;
-	if(cidr == 20):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 240;nmask[3] = 0;
-	if(cidr == 19):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 224;nmask[3] = 0;
-	if(cidr == 18):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 192;nmask[3] = 0;
-	if(cidr == 17):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 128;nmask[3] = 0;
-	if(cidr == 16):
-		nmask[0] = 255; nmask[1] = 255;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 15):
-		nmask[0] = 255; nmask[1] = 254;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 14):
-		nmask[0] = 255; nmask[1] = 252;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 13):
-		nmask[0] = 255; nmask[1] = 248;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 12):
-		nmask[0] = 255; nmask[1] = 240;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 11):
-		nmask[0] = 255; nmask[1] = 224;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 10):
-		nmask[0] = 255; nmask[1] = 192;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 9):
-		nmask[0] = 255; nmask[1] = 128;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 8):
-		nmask[0] = 255; nmask[1] = 0;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 7):
-		nmask[0] = 254; nmask[1] = 0;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 6):
-		nmask[0] = 252; nmask[1] = 0;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 5):
-		nmask[0] = 248; nmask[1] = 0;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 4):
-		nmask[0] = 240; nmask[1] = 0;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 3):
-		nmask[0] = 224; nmask[1] = 0;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 2):
-		nmask[0] = 192; nmask[1] = 0;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 1):
-		nmask[0] = 128; nmask[1] = 0;nmask[2] = 0;nmask[3] = 0;
-	if(cidr == 0):
-		nmask[0] = 0; nmask[1] = 0;nmask[2] = 0;nmask[3] = 0;
-		print('****** print nmask ******',nmask)
+	print('cidr', cidr, nmask)
+	cidr = int(cidr)
+	if cidr == 32:
+		nmask = [255,255,255,255]
+	if cidr == 31:
+		nmask = [255,255,255,254]
+	if cidr == 30:
+		nmask = [255,255,255,252]
+	if cidr == 32:
+		nmask = [255,255,255,255]
+	if cidr == 31:
+		nmask = [255,255,255,254]
+	if cidr == 30:
+		nmask = [255,255,255,252]
+	if cidr == 29:
+		nmask = [255,255,255,248]
+	if cidr == 28:
+		nmask = [255,255,255,240]
+	if cidr == 27:
+		nmask = [255,255,255,224]
+	if cidr == 26:
+		nmask = [255,255,255,192]
+	if cidr == 25:
+		nmask = [255,255,255,128]
+	if cidr == 24:
+		nmask = [255,255,255,0]
+	if cidr == 23:
+		nmask = [255,255,254,0]
+	if cidr == 22:
+		nmask = [255,255,252,0]
+	if cidr == 21:
+		nmask = [255,255,248,0]
+	if cidr == 20:
+		nmask = [255,255,240,0]
+	if cidr == 19:
+		nmask = [255,255,224,0]
+	if cidr == 18:
+		nmask = [255,255,192,0]
+	if cidr == 17:
+		nmask = [255,255,128,0]
+	if cidr == 16:
+		nmask = [255,255,0,0]
+	if cidr == 15:
+		nmask = [255,254,0,0]
+	if cidr == 14:
+		nmask = [255,252,0,0]
+	if cidr == 13:
+		nmask = [255,248,0,0]
+	if cidr == 12:
+		nmask = [255,240,0,0]
+	if cidr == 11:
+		nmask = [255,224,0,0]
+	if cidr == 10:
+		nmask = [255,192,0,0]
+	if cidr == 9:
+		nmask = [255,128,0,0]
+	if cidr == 8:
+		nmask = [255,0,0,0]
+	if cidr == 7:
+		nmask = [254,0,0,0]
+	if cidr == 6:
+		nmask = [252,0,0,0]
+	if cidr == 5:
+		nmask = [248,0,0,0]
+	if cidr == 4:
+		nmask = [240,0,0,0]
+	if cidr == 3:
+		nmask = [224,0,0,0]
+	if cidr == 2:
+		nmask = [192,0,0,0]
+	if cidr == 1:
+		nmask = [128,0,0,0]
+	if cidr == 0:
+		nmask = [0,0,0,0]
 	return nmask
+def getnetworkid(ipv4,nmask,netid):
+	for i in range (0,4):
+		netid[i] = (int(ipv4[i]) & int(nmask[i]))
+	return netid
 	
+def getbroadcast(nmask,netid,bcast):
+	imask = [-1,-1,-1,-1]
+	for i in range(0,4):
+		imask[i] = ~int(nmask[i])&255
+		bcast[i] = int(netid[i])^int(imask[i])
+	return bcast
+
+def getstartip(netid, startingip):
+	startingip = (netid[0], netid[1], netid[2], netid[3]+1)
+	return startingip
+
+def getendip(bcast, endingip):
+	endingip = (bcast[0], bcast[1], bcast[2], bcast[3]-1)
+	return endingip
+
+def gethostipa(startingip):
+	hostipa = (startingip[0], startingip[1], startingip[2], 0)
+	return hostipa
+	
+def gethostipb(endingip):
+	hostipb = (endingip[0], endingip[1], endingip[2], 255)
+	return hostipb
+
 def main():
 	cidr = -1
 	ipv4 = [-1,-1,-1,-1]
@@ -80,13 +112,22 @@ def main():
 	endingip = [-1,-1,-1,-1]
 	
 	print ('Input an IP and CIDR with 5 inputs.')
-	o1 = input("OCTET 1 : ")
-	o2 = input("OCTET 2 : ")
-	o3 = input("OCTET 3 : ")
-	o4 = input("OCTET 4 : ")
-	cidr = input("CIDR :")
-	print (o1,o2,o3,o4, "/", cidr)
-	ipv4[0] = o1; ipv4[1] = o2; ipv4[2] = o3;	ipv4[3] = o4;
+	ipv4[0] = input("OCTET 1: ")
+	ipv4[1] = input("OCTET 2: ")
+	ipv4[2] = input("OCTET 3: ")
+	ipv4[3] = input("OCTET 4: ")
+	cidr = input("CIDR: ")  
 	nmask = getsubnet(cidr,nmask)
-	print(ipv4,nmask,end=' ')
+	netid = getnetworkid(ipv4,nmask,netid)
+	bcast = getbroadcast(nmask,netid,bcast)
+	startingip = getstartip(netid, startingip)
+	endingip = getendip(bcast, endingip)
+	hostip1 = gethostipa(startingip)
+	hostip2 = gethostipb(endingip)
+	print("IP:",ipv4)
+	print("NETMASK:",nmask)
+	print("NETWORK ID:",netid)
+	print("BROADCAST ADDRESS:",bcast)
+	print("HOST IPS:",hostip1, "-", hostip2)
+	print("HOST RANGE:",startingip, "-", endingip)
 main()
